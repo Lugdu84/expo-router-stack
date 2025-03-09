6f154c174db80e6442142b60aefd9f7283630c34
David Grammatico <david.lugdunum@gmail.com>
2025-03-09 18:10:56 +0100

    first commit

---
+++ app/counter.tsx
@@ -0,0 +1,112 @@
+import { useState } from 'react';
+import { Button, Pressable, StyleSheet, Text, View } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { useRouter, Stack, useLocalSearchParams } from 'expo-router';
+
+export default function CounterScreen() {
+	const [count, setCount] = useState(0);
+	const router = useRouter();
+	const params = useLocalSearchParams();
+
+	const increment = () => setCount(count + 1);
+
+	const decrement = () => {
+		if (count > 0) {
+			setCount(count - 1);
+		}
+	};
+
+	const minusColor = count > 0 ? 'green' : 'red';
+
+	const getName = () => {
+		if (params.name) {
+			return ` de ${params.name}`;
+		}
+		return '';
+	};
+
+	const updateName = () => {
+		router.setParams({ name: 'David' });
+	};
+
+	return (
+		<View style={styles.container}>
+			<Stack.Screen
+				options={{
+					title: 'Counter',
+					headerStyle: {
+						backgroundColor: 'white',
+					},
+					headerTintColor: 'black',
+					headerLeft: () => (
+						<Pressable onPress={() => router.back()}>
+							<AntDesign
+								name="arrowleft"
+								size={24}
+								color="blue"
+							/>
+						</Pressable>
+					),
+					headerRight: () => (
+						<View style={styles.buttons}>
+							<Pressable onPress={decrement}>
+								<AntDesign
+									name="minuscircle"
+									size={24}
+									color={minusColor}
+								/>
+							</Pressable>
+							<Pressable onPress={increment}>
+								<AntDesign
+									name="pluscircle"
+									size={24}
+									color="green"
+								/>
+							</Pressable>
+						</View>
+					),
+				}}
+			/>
+			<Text style={styles.title}>Counter : {count} </Text>
+			<View style={styles.buttons}>
+				<Pressable onPress={decrement}>
+					<AntDesign
+						name="minuscircle"
+						size={24}
+						color={minusColor}
+					/>
+				</Pressable>
+				<Pressable onPress={increment}>
+					<AntDesign
+						name="pluscircle"
+						size={24}
+						color="green"
+					/>
+				</Pressable>
+			</View>
+			<Button
+				title="Update name"
+				onPress={updateName}
+			/>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 20,
+		gap: 50,
+		alignItems: 'center',
+	},
+	buttons: {
+		flexDirection: 'row',
+		justifyContent: 'space-evenly',
+		gap: 10,
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		textAlign: 'center',
+	},
+});
---
+++ app/index.tsx
@@ -0,0 +1,42 @@
+import { Link, Stack } from 'expo-router';
+import { useState } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+
+export default function Index() {
+	const [count, setCount] = useState(0);
+	return (
+		<View style={styles.container}>
+			<Stack.Screen
+				options={{
+					headerStyle: {
+						backgroundColor: 'blue',
+					},
+					title: `Expo Router : Stack`,
+				}}
+			/>
+			<Text>Leçon sur Expo Router : Stack</Text>
+			<Link
+				href={'/counter'}
+				asChild>
+				<Button title="counter" />
+			</Link>
+			<Link
+				href={{
+					pathname: '/counter',
+					params: { name: 'John' },
+				}}
+				asChild>
+				<Button title="counter with name" />
+			</Link>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		gap: 10,
+	},
+});
